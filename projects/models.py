0fab021154b16c282b4aa57442a77e77f64092d3
st666759 <sth.torres882@gmail.com>
2024-02-05 20:17:45 -0500

    ready for render

---
+++ projects/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    technology = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add = True)
